8d3f6b0abb5d7b5f6af18b79c1f3d645d43174cd
Juancho9912 <juanchovalenciaz@gmail.com>
2021-11-19 21:04:11 -0500

    Elementos de datsource, modelos, repositorios y relaciones

---
+++ src/models/rol.model.ts
@@ -0,0 +1,41 @@
+import {Entity, model, property, hasMany} from '@loopback/repository';
+import {Usuario} from './usuario.model';
+import {Permiso} from './permiso.model';
+import {PermisoRol} from './permiso-rol.model';
+
+@model()
+export class Rol extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  _id?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  nombre: string;
+
+  @property({
+    type: 'string',
+  })
+  usuarioId?: string;
+
+  @hasMany(() => Usuario)
+  usuarios: Usuario[];
+
+  @hasMany(() => Permiso, {through: {model: () => PermisoRol}})
+  permisos: Permiso[];
+
+  constructor(data?: Partial<Rol>) {
+    super(data);
+  }
+}
+
+export interface RolRelations {
+  // describe navigational properties here
+}
+
+export type RolWithRelations = Rol & RolRelations;
